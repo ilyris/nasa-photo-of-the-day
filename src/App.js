55afabb9e0b599794767cb70c2341a5262bc4b0a
Dylan Sieren <dylan.sieren01@gmail.com>
2019-10-24 16:16:45 -0500

    restyled it a little bit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import "./App.css";
 import NasaPOTD from './components/NasaPhotoOfTheDay';
 
 function App() {
-  //' Set our API data to slice of state.
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
       <NasaPOTD/>
     </div>
   );
--- src/components/NasaPhotoOfTheDay.jsx
+++ src/components/NasaPhotoOfTheDay.jsx
@@ -4,9 +4,7 @@ import S from 'styled-components';
 
 
 const NasaPhotoOfTheDay = () => {
-    // Create our state that we will need.   
-    // Data will be the Data our API call delivers to us.
-    
+    // Create todays date, dynamically so we can always have todays date be rendered.
     let today = new Date();
     let dd = today.getDate();
     let mm = today.getMonth() + 1; //January is 0!
@@ -18,33 +16,33 @@ const NasaPhotoOfTheDay = () => {
         mm = '0' + mm;
     } 
     today =  yyyy + '-' +   mm + '-' + dd ;
-    
+
+    // Create our state that we will need.   
+    // Data will be the Data our API call delivers to us.
     const [data, setData] = useState([]);
     // The dateValue will be the date that we recieve from the date input.
     const [dateValue, setDateValue] = useState(today);
 
     // the handleDate function will set the dateValue based on what the user selects.
-    const handleDate = event => {
-        setDateValue(event.target.value);
-    }
+    const handleDate = event => setDateValue(event.target.value);
   
   useEffect(() => {
-    const handleAPICall = dateValue => {
+    const handleAPICall = () => {
       axios.get(`https://api.nasa.gov/planetary/apod?date=${dateValue}&api_key=l160LWWTharY8cE8YDVDhY1S5ub7qAuSerze4Xu3`)
       .then( response => setData(response.data))
       .catch( error => console.log(error)
       );
     }
-    handleAPICall(dateValue);
+    handleAPICall();
   },[dateValue]);
-
-
 
     return(
         <StyledDiv>
             <RenderedState>This is to make sure my date renders since hooks are Async: {dateValue}</RenderedState>
-            <DateList type="date" onChange={handleDate} defaultValue={today} min="2019-01-01" max="2019-12-31"></DateList>
-            <NasaTitle >{data.title}</NasaTitle>
+            <TitleAndDateContainer>
+                <DateList type="date" onChange={handleDate} defaultValue={today} min="2019-01-01" max="2019-12-31"></DateList>
+                <NasaTitle primary >{data.title}</NasaTitle>
+            </TitleAndDateContainer>
             <NasaImage src={data.hdurl}></NasaImage>
             <NasaDescription>{data.explanation}</NasaDescription>
         </StyledDiv>
@@ -54,33 +52,61 @@ const NasaPhotoOfTheDay = () => {
 export default NasaPhotoOfTheDay;
 
 const StyledDiv = S.div`
-    width: 500px;
-    height: auto;
-    margin: 0 auto;
     display: flex;
     flex-direction: column;
-    background-color: #a7a7a7;
-    padding: 20px;
-    border-radius: 20px;
+    height: 100vh;
+    justify-content: space-between;
 `;
 const NasaTitle = S.h1`
-    font-size: 24px;
+    font-size: 70px;
     color: ${props => props.primary ? '#ffffff' : '#000'};
+    z-index: 10;
+    letter-spacing: 2px;
+    text-align: right;
+    margin: 0;
 `;
 const NasaDescription = S.p`
-    font-size: 18px;
-    color: #000;
+    font-size: 24px;
+    color: #fff;
+    z-index: 10;
+    position: relative;
+    bottom: 0;
+    margin: 0 auto;
+    width: 80%;
+    padding-bottm: 20px;
+    letter-spacing: 2px;
 `;
-
+const TitleAndDateContainer = S.div`
+    flex-direction: row;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+    justify-content: space-around;
+`;
 const NasaImage = S.img`
-    width: 500px;
-    height: auto;
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    left: 0;
+    top: 0;
 `;
 const DateList = S.input`
-    width: 150px;
+    width: 225px;
+    z-index: 10;
+    font-size: 22px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 20px;
 `;
 
 const RenderedState = S.p`
-    font-size: 20px;
-    color: #000;
+    font-size: 24px;
+    color: #fff;
+    z-index: 10;
+    position: absolute;
+    top: 0;
+    text-align: center;
+    width: 100%;
 `;
