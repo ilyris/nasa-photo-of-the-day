29877dd5839616388a88c8b31792aa5905f065b9
Dylan Sieren <dylan.sieren01@gmail.com>
2019-10-24 15:43:37 -0500

    moved all the state to the nasa component since we don't need to climb up the component structure to use our data, also create a today variable and passed that into the default state so we are able to get todays date all the time, then on change grab new date.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
-import axios from 'axios';
-import S from 'styled-components';
 import NasaPOTD from './components/NasaPhotoOfTheDay';
 
 function App() {
   //' Set our API data to slice of state.
-const [data, setData] = useState([]);
-const [dateValue, setDateValue] = useState('');
-
-const handleDate = event => {
-    setDateValue(event.target.value);
-    console.log(dateValue);
-    handleAPICall();
-
-}
-const handleAPICall = () => {
-  axios.get(`https://api.nasa.gov/planetary/apod?date=${dateValue}&api_key=l160LWWTharY8cE8YDVDhY1S5ub7qAuSerze4Xu3`)
-  .then( response => {
-    console.log(response.data);
-    setData(response.data);
-  })
-  .catch( error => {
-    console.log(error);
-  });
-}
-
-useEffect( () => {
-  handleAPICall();
-},[]);
-
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-      <NasaPOTD nasaData={data} handleDate={handleDate} dateValue={dateValue}/>
+      <NasaPOTD/>
     </div>
   );
 }
--- src/components/NasaPhotoOfTheDay.jsx
+++ src/components/NasaPhotoOfTheDay.jsx
@@ -1,17 +1,52 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 import S from 'styled-components';
 
 
-const NasaPhotoOfTheDay = ({nasaData,handleDate, dateValue}) => {
-
-
+const NasaPhotoOfTheDay = () => {
+    // Create our state that we will need.   
+    // Data will be the Data our API call delivers to us.
     
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+    let yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    } 
+    if (mm < 10) {
+        mm = '0' + mm;
+    } 
+    today =  yyyy + '-' +   mm + '-' + dd ;
+    
+    const [data, setData] = useState([]);
+    // The dateValue will be the date that we recieve from the date input.
+    const [dateValue, setDateValue] = useState(today);
+
+    // the handleDate function will set the dateValue based on what the user selects.
+    const handleDate = event => {
+        setDateValue(event.target.value);
+    }
+  
+  useEffect(() => {
+    const handleAPICall = dateValue => {
+      axios.get(`https://api.nasa.gov/planetary/apod?date=${dateValue}&api_key=l160LWWTharY8cE8YDVDhY1S5ub7qAuSerze4Xu3`)
+      .then( response => setData(response.data))
+      .catch( error => console.log(error)
+      );
+    }
+    handleAPICall(dateValue);
+  },[dateValue]);
+
+
+
     return(
         <StyledDiv>
-            <DateList onChange={handleDate} type="date" value={dateValue} min="2019-01-01" max="2019-12-31"></DateList>
-            <NasaTitle>{nasaData.title}</NasaTitle>
-            <NasaImage src={nasaData.hdurl}></NasaImage>
-            <NasaDescription>{nasaData.explanation}</NasaDescription>
+            <RenderedState>This is to make sure my date renders since hooks are Async: {dateValue}</RenderedState>
+            <DateList type="date" onChange={handleDate} defaultValue={today} min="2019-01-01" max="2019-12-31"></DateList>
+            <NasaTitle >{data.title}</NasaTitle>
+            <NasaImage src={data.hdurl}></NasaImage>
+            <NasaDescription>{data.explanation}</NasaDescription>
         </StyledDiv>
     );
 }
@@ -30,7 +65,7 @@ const StyledDiv = S.div`
 `;
 const NasaTitle = S.h1`
     font-size: 24px;
-    color: #fff;
+    color: ${props => props.primary ? '#ffffff' : '#000'};
 `;
 const NasaDescription = S.p`
     font-size: 18px;
@@ -43,4 +78,9 @@ const NasaImage = S.img`
 `;
 const DateList = S.input`
     width: 150px;
+`;
+
+const RenderedState = S.p`
+    font-size: 20px;
+    color: #000;
 `;
